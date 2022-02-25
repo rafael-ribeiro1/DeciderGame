54201dac8afe5866e3aabb6a723da18234063a15
Rafael Ribeiro <57731361+rafael-ribeiro1@users.noreply.github.com>
2022-02-25 13:07:08 +0000

    Initial design of MainActivity layout

--- app/src/main/java/pt/ipp/isep/decidergame/data/model/CalculusPair.kt
+++ app/src/main/java/pt/ipp/isep/decidergame/data/model/CalculusPair.kt
@@ -1,5 +1,6 @@
 package pt.ipp.isep.decidergame.data.model
 
+// TODO: Improve generation
 fun generatePair() : Pair<Calculus, Calculus> {
     val op1 = Operation.values().random()
     val val1 = if (op1 != Operation.DIV) (0..15).random() else (1..15).random()
